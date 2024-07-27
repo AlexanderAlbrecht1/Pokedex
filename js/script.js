ca7f126f446624966166995bdb01257092be611a
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-07-27 13:37:50 +0200

    first bg colours cards

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
 function init(){
-    fetchPokemon(1);
-    /*loadFirstPokemon(); */
-}
-
-async function fetchPokemon(x) {
-    let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${x}`);
-    let pokemonJson = await pokemon.json();
-
-    loadPokeCard(pokemonJson);
-}
-
-function loadPokeCard(pokemonJson) {
     document.getElementById('pokecardArea').innerHTML = ``;
-    let pokemonName = pokemonJson.name;
-    let pokemonPicture = pokemonJson.sprites.front_shiny;
-    let pokemonType1 = pokemonJson.types[0].type.name;
-    let pokemonType2 = pokemonJson.types[1].type.name;
-    console.log(pokemonName);
+    fetchPokemon();
+}
+
+async function fetchPokemon() {
+
+    for (let i = 1; i <= 151; i++) {
+        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        let pokemonJson = await pokemon.json();
+        loadPokeCard(pokemonJson,i);    
+    }   
+}
+
+async function loadPokeCard(pokemonJson,i) {
+   
+    let pokemonName = await pokemonJson.name;
+    let pokemonPicture = await pokemonJson.sprites.front_shiny;
+    let pokemonType = await pokemonJson.types[0].type.name;
+   
+    
 
     document.getElementById('pokecardArea').innerHTML += `
-    <div class="pokemonCard">
+    <div class="pokemonCard" id="pokemonCard${i}">
         <h3>${pokemonName}</h3>
         <img src="${pokemonPicture }" alt="">
             <div class="pokemonType">
-        <span>${pokemonType1}</span>
-        <span>${pokemonType2}</span>
+        <span>type: ${pokemonType}</span>
             </div>
     </div>
     `;
+    typeBackgroundColor(pokemonType,i);
+}
+
+function typeBackgroundColor(pokemonType,i) {
+    if (pokemonType =='water') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeWater)";
+    }
+    if (pokemonType =='grass') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeGrass)";
+    }
+    if (pokemonType =='fire') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeFire)";
+    }
+    if (pokemonType =='bug') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeBug)";
+    }
+    if (pokemonType =='normal') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeNormal)";
+    }
+    if (pokemonType =='poison') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typePoison)";
+    }
+    if (pokemonType =='electric') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeElectro)";
+    }
+    if (pokemonType =='ground') {
+        document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeGround)";
+    }
 }
