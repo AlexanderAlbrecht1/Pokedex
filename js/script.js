dc736b8073ef8982110d6528e4b8dc13477189d1
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-08-01 21:38:51 +0200

    load with types

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function init(){
 
 async function fetchPokemon() {
 
-    for (let i = 1; i <= 20; i++) {
+    for (let i = 1; i <= 300; i++) {
         let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         let pokemonJson = await pokemon.json();
         loadPokeCard(pokemonJson,i);    
@@ -17,22 +17,50 @@ async function loadPokeCard(pokemonJson,i) {
     let pokemonName = await pokemonJson.name;
     let pokemonPicture = await pokemonJson.sprites.front_shiny;
     let pokemonType = await pokemonJson.types[0].type.name;
+    if (await pokemonJson.types[1] === undefined) {
+        pokemonType2 = 'empty'
+    } else {
+        pokemonType2 = await pokemonJson.types[1].type.name  
+    }
+    
+    
    
     
 
     document.getElementById('pokecardArea').innerHTML += `
-    <div onclick="openBigPokemonCard('${pokemonName}','${pokemonPicture}','${pokemonType}')" class="pokemonCard" id="pokemonCard${i}">
-        <h3>${pokemonName}</h3>
-        <img src="${pokemonPicture}" alt="">
-            <div class="pokemonType">
-        <span>type: ${pokemonType}</span>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <h3>${pokemonName}</h3>
+                <img src="${pokemonPicture}" alt="">
+                    <div class="pokemonType"> 
+                        <span id="pokemonCard${i}" class="typeBox">${pokemonType}</span>
+                        <span id="pokemonType2${i}" class="typeBox">${pokemonType2}</span>
+                    </div>
             </div>
+            <div class="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect & Engineer</p>
+                <p>We love that guy</p>
+            </div>
+        </div>
     </div>
+
+    
     `;
-    typeBackgroundColorSmall(pokemonType,i);
+    checkType2(pokemonType2,i);
+    typeBackgroundColorSmall1(pokemonType,i);
+    typeBackgroundColorSmall2(pokemonType2,i);
+    
 }
 
-function typeBackgroundColorSmall(pokemonType,i) {
+function checkType2(pokemonType2,i) {
+    if (pokemonType2 == 'empty') {
+        document.getElementById(`pokemonType2${i}`).classList.add('none')
+    }
+}
+
+function typeBackgroundColorSmall1(pokemonType,i) {
     if (pokemonType =='water') {
         document.getElementById(`pokemonCard${i}`).style.backgroundColor = "var(--typeWater)";
     }
@@ -83,17 +111,72 @@ function typeBackgroundColorSmall(pokemonType,i) {
     }
 }
 
+function typeBackgroundColorSmall2(pokemonType,i) {
+    
+    if (pokemonType =='water') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeWater)";
+    }
+    if (pokemonType =='grass') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeGrass)";
+    }
+    if (pokemonType =='fire') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeFire)";
+    }
+    if (pokemonType =='bug') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeBug)";
+    }
+    if (pokemonType =='normal') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeNormal)";
+    }
+    if (pokemonType =='poison') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typePoison)";
+    }
+    if (pokemonType =='electric') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeElectro)";
+    }
+    if (pokemonType =='ground') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeGround)";
+    }
+    if (pokemonType =='flying') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeFlying)";
+    }
+    if (pokemonType =='rock') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeRock)";
+    }
+    if (pokemonType =='fighting') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeFighting)";
+    }
+    if (pokemonType =='ice') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeIce)";
+    }
+    if (pokemonType =='psychic') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typePsychic)";
+    }
+    if (pokemonType =='ghost') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeGhost)";
+    }
+    if (pokemonType =='dragon') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeDragon)";
+    }
+    if (pokemonType =='fairy') {
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeDragon)";
+    }
+}
+
  function openBigPokemonCard(pokemonName,pokemonPicture,pokemonType) {
     document.getElementById('BigCardContainer').classList.remove('none');
     document.getElementById('BigCardContainer').innerHTML = '';
     document.getElementById('BigCardContainer').innerHTML += `
         <div class="bigPokemonCard" id="bigPokemonCard">
-            <h4 onclick="closeBigPokemonCard()">Close Card</4>
+            <div class="closeSection"> 
+                <span onclick="closeBigPokemonCard()"> X </span>
+            </div>
             <h3>${pokemonName}</h3>
             <img src="${pokemonPicture}" alt="">
         </div>
     `;
     typeBackgroundColorBig(pokemonType);
+    disableScroll();
 } 
 
 function typeBackgroundColorBig(pokemonType) {
@@ -149,4 +232,13 @@ function typeBackgroundColorBig(pokemonType) {
 
 function closeBigPokemonCard() {
     document.getElementById('BigCardContainer').classList.add('none');
+    enableScroll();
+}
+
+function disableScroll() {
+    document.getElementById('html').classList.add('overflowHidden');
+}
+
+function enableScroll() {
+    document.getElementById('html').classList.remove('overflowHidden');
 }
