5f2065e5e4d78b1b07ab8cdc803ea96b1104d221
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-08-08 15:45:23 +0200

    new fetch

--- js/script.js
+++ js/script.js
@@ -1,67 +1,96 @@
+let pokedex = {};
+
 async function init() {
     document.getElementById('pokecardArea').innerHTML = ``;
-    await fetchPokemon();
-}
-
-async function fetchPokemon() {
-
-    for (let i = 1; i <= 20; i++) {
-        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-        let pokemonJson = await pokemon.json();
-        await loadPokeCard(pokemonJson, i);
-    }
-}
-
-async function loadPokeCard(pokemonJson, i) {
-
-    let pokemonName = await pokemonJson.name;
-    let pokemonPicture = await pokemonJson.sprites.front_shiny;
-    let pokemonID = await pokemonJson.id;
-    let pokemonPictureBack = await pokemonJson.sprites.back_shiny;
-    let pokemonType = await pokemonJson.types[0].type.name;
-    if (await pokemonJson.types[1] === undefined) {
-        pokemonType2 = 'empty'
-    } else {
-        pokemonType2 = await pokemonJson.types[1].type.name
-    }
-
-
-
-
-
-    document.getElementById('pokecardArea').innerHTML += `
-    <div class="flip-card">
-        <div class="flip-card-inner">
-            <div class="flip-card-front">
-                <h3>${pokemonName}</h3>
-                <span><b>id: ${pokemonID}</b></span>
-                <img src="${pokemonPictureBack}" alt="
-                Oops... unfortunately you lost the Pokemon">
-                    <div class="pokemonType"> 
-                        <span id="pokemonCard${i}" class="typeBox">${pokemonType}</span>
-                        <span id="pokemonType2${i}" class="typeBox">${pokemonType2}</span>
-                    </div>
-            </div>
-            <div class="flip-card-back">
-                <h3>${pokemonName}</h3>
-                <span class="cb"><b>id: ${pokemonID}</b></span>
-                <img src="${pokemonPicture}" alt="
-                Oops... unfortunately you lost the Pokemon">
-                <button onclick="openBigPokemonCard('${pokemonName}','${pokemonType}')" type="button" class="btn btn-secondary btn-lg btn-block mb-2">View details</button>
-            </div>
-        </div>
-    </div>
-
+    showLoadingSpinner();
+    await fetchPokemonData();
+    console.log(pokedex);
+    console.log({pokedex}.length);
+    loadPokeCard();
+    console.log('done');
+    hideLoadingSpinner();
     
+}
+
+async function fetchPokemonData() {
+    const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+    // Funktion zum Laden der Daten eines einzelnen Pokémon
+    async function getPokemonData(id) {
+        const response = await fetch(`${baseUrl}${id}`);
+        const data = await response.json();
+        return data;
+    }
+
+    // Iteriere über die Pokémon-IDs von 1 bis 1302
+    for (let id = 1; id <= 151; id++) {
+        try {
+            const data = await getPokemonData(id);
+            // Speichere die relevanten Daten im Pokedex
+            pokedex[id] = {
+                name: data.name,
+                id: data.id,
+                height: data.height,
+                weight: data.weight,
+                types: data.types.map(typeInfo => typeInfo.type.name),
+                abilities: data.abilities.map(abilityInfo => abilityInfo.ability.name),
+                stats: data.stats.map(statInfo => ({
+                    name: statInfo.stat.name,
+                    value: statInfo.base_stat
+                })),
+                sprites: {
+                    front_shiny: data.sprites.front_shiny,
+                    back_shiny: data.sprites.back_shiny,
+                    dream_world: data.sprites.other.dream_world.front_default,
+                }
+            };
+        } catch (error) {
+            console.error(`Fehler beim Laden der Daten für Pokémon mit ID ${id}:`, error);
+        }
+    }
+
+    return pokedex;
+}
+
+
+function loadPokeCard() {
+    
+    for (let i = 1; i < 25 ; i++) {
+
+        document.getElementById('pokecardArea').innerHTML += `
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <h3>${pokedex[i].name}</h3>
+                        <span><b>id: ${pokedex[i].id}</b></span>
+                        <img src="${pokedex[i].sprites.back_shiny}" alt="Oops... unfortunately you lost the Pokemon">
+                        <div class="pokemonType"> 
+                            <span id="pokemonCard${i}" class="typeBox">${pokedex[i].types[0]}</span>
+                            <span id="pokemonType2${i}" class="typeBox">${pokedex[i].types[1]}</span>
+                        </div>
+                    </div> 
+                    <div class="flip-card-back">
+                        <h3>${pokedex[i].name}</h3>
+                        <span class="cb"><b>id: ${pokedex[i].id}</b></span>
+                        <img src="${pokedex[i].sprites.front_shiny}" alt="Oops... unfortunately you lost the Pokemon">
+                        <button onclick="openBigPokemonCard(${i})" type="button" class="btn btn-secondary btn-lg btn-block mb-2">View details</button>
+                    </div>
+                </div>
+            </div> 
     `;
-    checkType2(pokemonType2, i);
-    typeBackgroundColorSmall1(pokemonType, i);
-    typeBackgroundColorSmall2(pokemonType2, i);
+    checkType2(pokedex[i].types[1], i);
+    typeBackgroundColorSmall1(pokedex[i].types[0], i);
+    typeBackgroundColorSmall2(pokedex[i].types[1], i);
+    }
+
+    console.log('done2');
+      
 
 }
 
 function checkType2(pokemonType2, i) {
-    if (pokemonType2 == 'empty') {
+    if (pokemonType2 == undefined) {
         document.getElementById(`pokemonType2${i}`).classList.add('none')
     }
 }
@@ -169,17 +198,38 @@ function typeBackgroundColorSmall2(pokemonType, i) {
     }
 }
 
-function openBigPokemonCard(pokemonName, pokemonType) {
+
+
+async function openBigPokemonCard(i) {
+    let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    let pokemonJson = await pokemon.json();
+    let pokemonType = await pokemonJson.types[0].type.name;
     document.getElementById('BigCardContainer').classList.remove('none');
     document.getElementById('BigCardContainer').innerHTML = '';
     document.getElementById('BigCardContainer').innerHTML += `
-        <div class="bigPokemonCard" id="bigPokemonCard">
-            <div class="closeSection"> 
-                <span onclick="closeBigPokemonCard()"> X </span>
+    <div class="bigPokemonCard" id="bigPokemonCard">
+    <div class="closeSection"> 
+        <img onclick="closeBigPokemonCard()" src="img/xmark-solid.svg" alt="close">
+    </div>
+    <h3>${pokemonJson.name}</h3>
+    <div class="evolution">
+        <img class="evo1" src="${pokedex[i].sprites.dream_world}" alt="">
+    </div>
+    <div id="dataBG">
+        <div id="data"
+            <div class="size">
+                <span>weight: ${pokemonJson.weight} kg</span>
+                <span>height: ${pokemonJson.height} m</span>
             </div>
-            <h3>${pokemonName}</h3>
-            
         </div>
+        <div class="arrows">
+            <img onclick="previouslyPokemon(${i})" src="img/circle-arrow-left-solid.svg" alt="back">
+            <img onclick="nextPokemon(${i})" src="img/circle-arrow-right-solid.svg" alt="forward">
+        </div>
+    </div>
+    
+    
+</div>
     `;
     typeBackgroundColorBig(pokemonType);
     disableScroll();
@@ -241,6 +291,16 @@ function closeBigPokemonCard() {
     enableScroll();
 }
 
+function nextPokemon(i) {
+    i++
+    openBigPokemonCard(i)
+}
+
+function previouslyPokemon(i) {
+    i--
+    openBigPokemonCard(i)
+}
+
 function disableScroll() {
     document.getElementById('html').classList.add('overflowHidden');
 }
@@ -249,16 +309,16 @@ function enableScroll() {
     document.getElementById('html').classList.remove('overflowHidden');
 }
 
+function showLoadingSpinner() {
+    document.querySelector("main").style.visibility = "hidden";
+    document.getElementById("loading").style.visibility = "visible";
+    disableScroll();
+}
 
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector("main").style.visibility = "hidden";
-        document.getElementById("loading").style.visibility = "visible";
-    } else {
-        setTimeout(() => {
-            document.getElementById("loading").style.display = "none";
-            document.querySelector("main").style.visibility = "visible";
-        }, 3000)
-    }
-}; 
+function hideLoadingSpinner() {
+    document.getElementById("loading").style.display = "none";
+    document.querySelector("main").style.visibility = "visible";
+    enableScroll();
+}
+
 
