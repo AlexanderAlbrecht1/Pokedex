b4f56b8323e72815f3cdf724b46505bcca217e58
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-08-13 23:19:12 +0200

    little steps

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-let pokedex = {};
+let pokedex = [];
 let currentIndex = 0;
 let results = [];
 
 
 async function init() {
-    showLoadingSpinner();
-    document.getElementById('pokecardArea').innerHTML = ``;   
+
+    document.getElementById('pokecardArea').innerHTML = ``;
     await fetchPokemonData();
-    console.log([pokedex].length);
+
     loadPokeCard();
-    hideLoadingSpinner();
-    
+
+
 }
 
 async function fetchPokemonData() {
@@ -50,9 +50,10 @@ async function fetchPokemonData() {
 
 
 function loadPokeCard() {
-    let loadCount = 20; 
+    showLoadingSpinner();
+    let loadCount = 20;
     let endIndex = Math.min(currentIndex + loadCount, Object.keys(pokedex).length);
-    for (let i = currentIndex+1; i <= endIndex; i++) {
+    for (let i = currentIndex + 1; i <= endIndex; i++) {
 
         document.getElementById('pokecardArea').innerHTML += `
             <div class="flip-card">
@@ -75,17 +76,18 @@ function loadPokeCard() {
                 </div>
             </div> 
     `;
-    checkType2(pokedex[i].types[1], i);
-    typeBackgroundColorSmall1(pokedex[i].types[0], i);
-    typeBackgroundColorSmall2(pokedex[i].types[1], i);
+        checkType2(pokedex[i].types[1], i);
+        typeBackgroundColorSmall1(pokedex[i].types[0], i);
+        typeBackgroundColorSmall2(pokedex[i].types[1], i);
     }
 
     currentIndex = endIndex; // Update den aktuellen Index
-      
+
     // Überprüfe, ob alle Pokémon geladen sind, und deaktiviere den Button, wenn dies der Fall ist
     if (currentIndex >= Object.keys(pokedex).length) {
         document.getElementById('loadMorePokemonButton').classList.add('none');
     }
+    hideLoadingSpinner();
 }
 
 function checkType2(pokemonType2, i) {
@@ -225,36 +227,50 @@ function typeBackgroundColorSmall2(pokemonType, i) {
         document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeDragon)";
     }
     if (pokemonType == 'fairy') {
-        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeDragon)";
+        document.getElementById(`pokemonType2${i}`).style.backgroundColor = "var(--typeFairy)";
     }
 }
 
 
 
 async function openBigPokemonCard(i) {
-    let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    let pokemonJson = await pokemon.json();
-    let pokemonType = await pokemonJson.types[0].type.name;
+    let pokemon = pokedex[i];
     document.getElementById('BigCardContainer').classList.remove('none');
     document.getElementById('BigCardContainer').innerHTML = '';
+
+    // Erzeuge HTML für die Statistiken
+    let statsHTML = '';
+
+    for (let x = 0; x < pokemon.stats.length; x++) {
+        statsHTML += `
+            <div class="stat-bar">
+                <div class="stat-label">${pokemon.stats[x].name}:</div>
+                <div class="stat-value" id="stat-value${x}" style="width: ${pokemon.stats[x].value}px;">
+                    <span>${pokemon.stats[x].value}</span>
+                </div>
+            </div>
+        `;
+    }
+
     document.getElementById('BigCardContainer').innerHTML += `
     <div class="bigPokemonCard" id="bigPokemonCard">
-    <div class="closeSection"> 
-        <img onclick="closeBigPokemonCard()" src="img/xmark-solid.svg" alt="close">
-    </div>
-    <h3>${pokemonJson.name}</h3>
+    <h3>${pokedex[i].name}</h3>
     <div class="evolution">
         <img class="evo1" src="${pokedex[i].sprites.dream_world}" alt="">
     </div>
     <div id="dataBG">
         <div id="data"
             <div class="size">
-                <span>weight: ${pokemonJson.weight} kg</span>
-                <span>height: ${pokemonJson.height} m</span>
+                <span>weight: ${pokedex[i].weight} kg</span>
+                <span>height: ${pokedex[i].height} m</span>
+            </div>
+            <div id="statsContainer${i}" class="stats-container">
+            ${statsHTML}
             </div>
         </div>
         <div class="arrows">
             <img onclick="previouslyPokemon(${i})" src="img/circle-arrow-left-solid.svg" alt="back">
+            <img onclick="closeBigPokemonCard()" src="img/xmark-solid.svg" alt="close">
             <img onclick="nextPokemon(${i})" src="img/circle-arrow-right-solid.svg" alt="forward">
         </div>
     </div>
@@ -262,7 +278,7 @@ async function openBigPokemonCard(i) {
     
 </div>
     `;
-    typeBackgroundColorBig(pokemonType);
+    typeBackgroundColorBig(pokedex[i].types[0], i);
     disableScroll();
 }
 
@@ -323,13 +339,35 @@ function closeBigPokemonCard() {
 }
 
 function nextPokemon(i) {
-    i++
-    openBigPokemonCard(i)
+    i++;
+    if (i >= pokedex.length) {
+        i = 1;
+        openBigPokemonCard(i);
+    } else {
+        openBigPokemonCard(i);
+    }
+    
+}
+
+function nextResultPokemon(i) {
+    i++;
+    if (i >= results.length) {
+        i = 0; 
+        openBigResultCard(i);   
+    } else {
+        openBigResultCard(i);
+    }
+    
 }
 
 function previouslyPokemon(i) {
-    i--
-    openBigPokemonCard(i)
+    i--;
+    openBigPokemonCard(i);
+}
+
+function previouslyResultPokemon(i) {
+    i--;
+    openBigResultCard(i);
 }
 
 function disableScroll() {
@@ -341,14 +379,14 @@ function enableScroll() {
 }
 
 function showLoadingSpinner() {
-    document.querySelector("main").style.visibility = "hidden";
-    document.getElementById("loading").style.visibility = "visible";
+    document.getElementById('main').style.visibility = "hidden";
+    document.getElementById('loading').style.visibility = "visible";
     disableScroll();
 }
 
 function hideLoadingSpinner() {
-    document.getElementById("loading").style.display = "none";
-    document.querySelector("main").style.visibility = "visible";
+    document.getElementById('loading').style.visibility = "hidden";
+    document.getElementById('main').style.visibility = "visible";
     enableScroll();
 }
 
@@ -356,7 +394,7 @@ function searchAndDisplayPokemons() {
     let query = document.getElementById('searchInput').value.trim().toLowerCase();
     results = [];
     if (query.length >= 3) {
-        
+
         let searchResults = searchPokemonByName(query);
         displaySearchResults(searchResults);
     } else {
@@ -365,13 +403,13 @@ function searchAndDisplayPokemons() {
 }
 
 function searchPokemonByName(query) {
-    
+
     for (let i = 1; i < Object.keys(pokedex).length; i++) {
         if (pokedex[i].name.toLowerCase().includes(query)) {
             results.push(pokedex[i]);
         }
     }
-    
+
     return results;
 }
 
@@ -396,21 +434,71 @@ function displaySearchResults(pokemons) {
                         <h3>${results[i].name}</h3>
                         <span class="cb"><b>id: ${results[i].id}</b></span>
                         <img src="${results[i].sprites.front_shiny}" alt="Oops... unfortunately you lost the Pokemon">
-                        <button onclick="openBigPokemonCard(${i})" type="button" class="btn btn-secondary btn-lg btn-block mb-2">View details</button>
+                        <button onclick="openBigResultCard(${i})" type="button" class="btn btn-secondary btn-lg btn-block mb-2">View details</button>
                     </div>
                 </div>
             </div> 
     `;
-    checkType2(results[i].types[1], i);
-    typeBackgroundColorSmall1(results[i].types[0], i);
-    typeBackgroundColorSmall2(results[i].types[1], i);
-    
+            checkType2(results[i].types[1], i);
+            typeBackgroundColorSmall1(results[i].types[0], i);
+            typeBackgroundColorSmall2(results[i].types[1], i);
+
         }
     } else {
         document.getElementById('pokecardArea').innerHTML = `<p>No Pokémon found matching "${document.getElementById('searchInput').value}".</p>`;
     }
 
-    
+
 }
+
+function openBigResultCard(i) {
+    let pokemon = results[i];
+    document.getElementById('BigCardContainer').classList.remove('none');
+    document.getElementById('BigCardContainer').innerHTML = '';
+
+    // Erzeuge HTML für die Statistiken
+    let statsHTML = '';
+
+    for (let x = 0; x < pokemon.stats.length; x++) {
+        statsHTML += `
+            <div class="stat-bar">
+                <div class="stat-label">${pokemon.stats[x].name}:</div>
+                <div class="stat-value" id="stat-value${x}" style="width: ${pokemon.stats[x].value}px;">
+                    <span>${pokemon.stats[x].value}</span>
+                </div>
+            </div>
+        `;
+    }
+
+    document.getElementById('BigCardContainer').innerHTML += `
+    <div class="bigPokemonCard" id="bigPokemonCard">
+    <h3>${pokemon.name}</h3>
+    <div class="evolution">
+        <img class="evo1" src="${pokemon.sprites.dream_world}" alt="">
+    </div>
+    <div id="dataBG">
+        <div id="data"
+            <div class="size">
+                <span>weight: ${pokemon.weight} kg</span>
+                <span>height: ${pokemon.height} m</span>
+            </div>
+            <div id="statsContainer${i}" class="stats-container">
+            ${statsHTML}
+            </div>
+        </div>
+        <div class="arrows">
+            <img onclick="previouslyResultPokemon(${i})" src="img/circle-arrow-left-solid.svg" alt="back">
+            <img onclick="closeBigPokemonCard()" src="img/xmark-solid.svg" alt="close">
+            <img onclick="nextResultPokemon(${i})" src="img/circle-arrow-right-solid.svg" alt="forward">
+        </div>
+    </div>
+    
+    
+</div>
+    `;
+    typeBackgroundColorBig(pokemon.types[0], i);
+    disableScroll();
+}
+
 
 
